99b74bb936d8c2244c02ad279dc41197fc9c80f6
ansu kushvah <162787774+aansu18@users.noreply.github.com>
2026-08-18 15:21:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste your Supabase project details here.
 // Find these in your Supabase project: Settings -> API
-const SUPABASE_URL = "https://https://hsqjwyztsqacxylwqfle.supabase.co";
+const SUPABASE_URL = "https://hsqjwyztsqacxylwqfle.supabase.co";
 const SUPABASE_ANON_KEY = 
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzcWp3eXp0c3FhY3h5bHdxZmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMzQ0NzcsImV4cCI6MjEwMjYxMDQ3N30.KIrp_zjwPSg0aBO6Z0BRds-JTK2c04xsk7UaKnwhRBI";
