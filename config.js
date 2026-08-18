80b145756ea1303f7fed6f81c977b4aaa208d05f
ansu kushvah <162787774+aansu18@users.noreply.github.com>
2026-08-18 13:16:14 +0530

    Update Supabase project details in config.js
    Updated Supabase URL and anon key for the project.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Paste your Supabase project details here.
 // Find these in your Supabase project: Settings -> API
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+const SUPABASE_URL = "https://https://hsqjwyztsqacxylwqfle.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzcWp3eXp0c3FhY3h5bHdxZmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMzQ0NzcsImV4cCI6MjEwMjYxMDQ3N30.KIrp_zjwPSg0aBO6Z0BRds-JTK2c04xsk7UaKnwhRBI
+";
